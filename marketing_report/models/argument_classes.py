8f973409e6815972f70911cc21be77cd59fa0d39
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-08-25 15:43:59 +0300

    initial commit

---
+++ marketing_report/models/argument_classes.py
@@ -0,0 +1,40 @@
+import abc
+import json
+
+
+class ArgumentTypes(abc.ABC):
+    def __init__(self, code, description):
+        self.code = code
+        self.description = description
+
+    def to_dict(self):
+        return {'code': self.code, 'description': self.description}
+
+
+class MoneyTypes(ArgumentTypes):
+    pass
+
+
+class TimeTypes(ArgumentTypes):
+    pass
+
+
+def money_argument_list():
+    return [MoneyTypes('SalesVat', 'Продажи с НДС'),
+            MoneyTypes('SalesNoVat', 'Продажи без НДС'),
+            MoneyTypes('Profit', 'Прибыль'),
+            MoneyTypes('SalesQuantity', 'Количество продаж')]
+
+
+def time_argument_list():
+    return [TimeTypes('1', '1 год'),
+            TimeTypes('2', '2 года'),
+            TimeTypes('3', '3 года')]
+
+
+def json_money_argument_list():
+    return json.dumps([report.to_dict() for report in money_argument_list()], ensure_ascii=False)
+
+
+def json_time_argument_list():
+    return json.dumps([report.to_dict() for report in time_argument_list()], ensure_ascii=False)
