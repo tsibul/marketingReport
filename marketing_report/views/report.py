b4e3cfd26d7ee99d30b08e8b3a6a381a21e375ef
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-08-25 08:27:31 +0300

    template for report blocks

--- marketing_report/views/report.py
+++ marketing_report/views/report.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from marketing_report.models.report_period import *
 
 
-def reports (request):
-    context = {}
+def reports(request):
+    periods = ReportPeriod.calculable_list()
+    context = {'periods': periods}
     return render(request, 'report.html', context)
-
