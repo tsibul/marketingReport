337367d8f8fb5a21a893d0dafd6de95ce1697c51
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-08-27 15:19:36 +0300

    dictionary & imports initial commit

--- marketing_report/views/__init__.py
+++ marketing_report/views/__init__.py
@@ -1,3 +1,5 @@
 from marketing_report.views.index import *
 from marketing_report.views.report import *
+from marketing_report.views.dictionary import *
+from marketing_report.views.imports import *
 
