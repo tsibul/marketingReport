cb1aa6c16d50aae4147f6ba10d893319b97eb9d1
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-09-10 22:07:37 +0300

    dictionary change line to form in html function dictionary change  back form to line in html function dictionary for dictionary type (to be changed) — dictList prepare universal function for dictionary update

---
+++ marketing_report/static/marketing_report/js/dictionary.js
@@ -0,0 +1,105 @@
+const dictList = {
+    matrix: 'matrix',
+    crm: 'crm',
+    printType: 'print_type',
+    colorScheme: 'color_scheme',
+    color: 'color',
+    customer: 'customer',
+    customerType: 'customer_type',
+    customerGroup: 'customer_group',
+    goods: 'goods',
+};
+
+
+document.addEventListener('mousedown', function (element) {
+    try {
+        if (element.target !== document.querySelector('form').parentElement &&
+            !document.querySelector('form').parentElement.contains(element.target)) {
+            const buttonClose = document.querySelector('form').querySelector('.btn-close');
+            cancelEditDictionary(buttonClose);
+        }
+    } catch (exception) {
+    }
+});
+
+function editDictionary(obj) {
+    const nodeElements = obj.childNodes;
+    const newNode = document.createElement('form'); // block for new row
+    newNode.classList.add('form-row');
+    newNode.id = 'form-dict';
+    let childNode;
+    let changes = 0;
+    nodeElements.forEach(function (node) {
+        if (node.tagName === 'DIV' && !node.hidden) {
+            childNode = document.createElement('input'); // block for input
+            childNode.classList.add('form-input', 'dict-section-block-text');
+            childNode.type = 'text';
+            childNode.name = node.dataset.name;
+            childNode.setAttribute('value', node.textContent);
+            newNode.appendChild(childNode)
+            changes += 1;
+        }
+        node.hidden = true;
+    });
+    if (changes === 0) {
+        return
+    }
+    obj.querySelector('.id-hidden').setAttribute('form', 'form-dict')
+    newNode.appendChild(createButtonBlock());
+    obj.appendChild(newNode);
+}
+
+function cancelEditDictionary(obj) {
+    const parentObj = obj.parentElement.parentElement;
+    const row = parentObj.parentElement;
+    parentObj.remove();
+    const elementId = row.dataset.id;
+    if (elementId === '') {
+        row.remove();
+        return;
+    }
+    row.childNodes.forEach(function (element) {
+        if (element.hidden) {
+            element.hidden = false
+        }
+    });
+    row.querySelector('.id-hidden').setAttribute('form', '');
+}
+
+function createButtonBlock() {
+    /* create button block for buttons submit & cancel */
+    let childNode;
+    const buttonBlock = document.createElement('div');
+    buttonBlock.classList.add('dict__btn-block'); // block for buttons submit & cancel
+    childNode = document.createElement('button'); //button cancel
+    childNode.innerHTML = '<i class="fa fa-solid fa-xmark" ></i>';
+    childNode.classList.add('btn', 'btn-close', 'dict__btn');
+    childNode.setAttribute('onclick', 'event.stopPropagation(); cancelEditDictionary(this);');
+    childNode.type = 'button';
+    buttonBlock.appendChild(childNode);
+    childNode = document.createElement('button'); // button submit
+    childNode.innerHTML = '<i class="fa fa-solid fa-check"></i>';
+    childNode.classList.add('btn', 'btn-save', 'dict__btn');
+    childNode.setAttribute('onclick', 'event.stopPropagation(); saveDictionaryRecord(this);');
+    childNode.type = 'submit';
+    buttonBlock.appendChild(childNode);
+    return buttonBlock;
+}
+
+function saveDictionaryRecord(obj) {
+    event.preventDefault();
+    const updateForm = obj.parentElement.parentElement;
+    const dictionaryType = updateForm.parentElement.id.split('_')[0];
+    const fetchPath = '/dict_update/' + dictList[dictionaryType];
+    const formData = new FormData(updateForm);
+    fetch(fetchPath, {
+        method: 'POST',
+        body: formData,
+    })
+        .then(async () => {
+            let fieldList;
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
