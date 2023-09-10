8e426c20740357411ca6f03ffcdd895891956878
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-09-10 13:07:19 +0300

    include dictionary databases

--- marketing_report/views/dictionary.py
+++ marketing_report/views/dictionary.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from marketing_report.models.color_models import PrintType, ColorScheme
+from marketing_report.models.goods_types_models import GoodCrmType, GoodMatrixType
 
 
 def dictionary(request):
     navi = 'dictionary'
-    context = {'navi': navi}
+    matrix = GoodMatrixType.objects.all().order_by('matrix_name')
+    crm = GoodCrmType.objects.all().order_by('crm_name')
+    print_type = PrintType.objects.all().order_by('type_name')
+    color_group = ColorScheme.objects.all().order_by('scheme_name')
+    context = {'navi': navi, 'matrix': matrix, 'crm': crm, 'print_type': print_type, 'color_group': color_group}
     return render(request, 'dictionary.html', context)
