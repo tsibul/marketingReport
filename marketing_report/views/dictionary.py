fc09244f4c4e0c0e411cf71262e783c6cff1d087
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-09-17 00:45:11 +0300

    add color

--- marketing_report/views/dictionary.py
+++ marketing_report/views/dictionary.py
@@ -18,9 +18,12 @@ def dictionary(request):
     customer_group_end = CustomerGroups.objects.all().order_by('group_name')[19:20]
     customer = Customer.objects.filter(internal=False).order_by('name')[0:19]
     customer_end = Customer.objects.filter(internal=False).order_by('name')[19:20]
+    color = Color.objects.all().order_by('color_scheme', 'color_id')[0:19]
+    color_end = Color.objects.all().order_by('color_scheme', 'color_id')[19:20]
     context = {'navi': navi, 'matrix': matrix, 'crm': crm, 'print_type': print_type, 'color_group': color_group,
-               'customer_type': customer_type, 'customer_group': customer_group,
-               'customer_group_end': customer_group_end, 'customer': customer, 'customer_end': customer_end}
+               'customer_type': customer_type, 'customer_group': customer_group, 'color': color,
+               'color_end': color_end, 'customer_group_end': customer_group_end, 'customer': customer,
+               'customer_end': customer_end}
     return render(request, 'dictionary.html', context)
 
 
