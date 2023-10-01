7b4d5488328ae58587dc64f800fc570b7a359647
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-10-01 17:36:41 +0300

    importCustomers model

--- marketing_report/models/__init__.py
+++ marketing_report/models/__init__.py
@@ -3,3 +3,4 @@ from marketing_report.models.customer_group_models import *
 from marketing_report.models.customer_models import *
 from marketing_report.models.goods_types_models import *
 from marketing_report.models.goods_models import *
+from marketing_report.models.import_models import *
---
+++ marketing_report/models/import_models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class ImportCustomers (models.Model):
+    frigat_id = models.IntegerField(db_index=True)
+    form = models.CharField(max_length=255, blank=True)
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255, blank=True)
+    inn = models.CharField(max_length=20, null=True)
+    customer_type = models.CharField(max_length=255, null=True, blank=True)
+    phone = models.CharField(max_length=255, blank=True)
+    all_phones = models.CharField(max_length=600, null=True, blank=True)
+    mail = models.CharField(max_length=255, null=True, blank=True)
+    all_mails = models.CharField(max_length=600, null=True, blank=True)
+    comment = models.CharField(max_length=255, null=True, blank=True)
+    our_manager = models.CharField(max_length=255, blank=True)
+
