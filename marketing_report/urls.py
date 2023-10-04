b8c0e3a154217af4633b07b350223d3fa5fb180c
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-10-04 22:44:00 +0300

    goods eco export

--- marketing_report/urls.py
+++ marketing_report/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('report/', views.reports, name='reports'),
     path('json_report/<int:report_no>/<str:report_type>/<str:period>/<str:date_begin>/<str:date_end>/<str:argument>',
          views.json_periods, name='json_report'),
+
     path('customer/', views.customer, name='customer'),
 
     path('dictionary/', views.dictionary, name='dictionary'),
@@ -16,9 +17,13 @@ urlpatterns = [
     path('dict_delete/<str:dict_type>/<int:id_no>', views.dictionary_delete, name='dictionary_delete'),
     path('json_dict_next_20/<str:dict_type>/<int:id_no>/<str:order>/<str:search_string>', views.dictionary_json,
          name='dictionary_json'),
+
     path('imports/', views.imports, name='imports'),
+    path('import_file/', views.import_file, name='import_file'),
+
     path('admin/', views.admin, name='admin_site'),
     path('customer_export/', views.customer_export, name='customer_export'),
+    path('goods_export/', views.goods_export, name='goods_export'),
     path('customer_group_json/', views.customer_group_json, name='customer_group_json'),
 
 ]
--- marketing_report/views/adminsite.py
+++ marketing_report/views/adminsite.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from datetime import datetime
 
-from marketing_report.models import Customer
+from marketing_report.models import Customer, Goods
 
 
 def admin(request):
@@ -21,7 +21,15 @@ def customer_export(request):
     customer = Customer.objects.filter(date_last__gte=date_last, internal=False).values_list('frigat_id', 'name',
                                                                                              'customer_type__code',
                                                                                              'customer_type__type_name')
-    with open('customers.csv', 'w', newline='') as export_file:
+    with open('customer_export.csv', 'w', newline='') as export_file:
         writer = csv.writer(export_file)
         writer.writerows(customer)
+    return HttpResponseRedirect(reverse('marketing_report:admin_site'))
+
+
+def goods_export(request):
+    goods = Goods.objects.filter(deleted=False).values_list('name', 'item_name', 'eco')
+    with open('goods_export.csv', 'w', newline='') as export_file:
+        writer = csv.writer(export_file)
+        writer.writerows(goods)
     return HttpResponseRedirect(reverse('marketing_report:admin_site'))
