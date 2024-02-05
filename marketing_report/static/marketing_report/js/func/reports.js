8ff6660a3fcd076811054c0016fc9d003b6b274d
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-02-06 02:14:44 +0300

    moved to functions then export from it

--- marketing_report/static/marketing_report/js/func/reports.js
+++ marketing_report/static/marketing_report/js/func/reports.js
@@ -1,20 +1,21 @@
+'use strict'
 // parse json on main page
-function periodsList() {
+export function periodsList() {
     return JSON.parse(document.getElementById('periods').textContent);
 }
 
-function customerReports() {
+export function customerReports() {
     return JSON.parse(document.getElementById('json-cst-reports').textContent);
 }
 
-function goodsReports() {
+export function goodsReports() {
     return JSON.parse(document.getElementById('json-goods-reports').textContent);
 }
 
-function moneyArguments() {
+export function moneyArguments() {
     return JSON.parse(document.getElementById('json-money-arguments').textContent);
 }
 
-function timeArguments() {
+export function timeArguments() {
     return JSON.parse(document.getElementById('json-time-arguments').textContent);
 }
