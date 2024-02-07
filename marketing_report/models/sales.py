e019577561afa590db8a1dcbf4774a12aa6cd335
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-02-07 23:26:41 +0300

    change field names

--- marketing_report/models/sales.py
+++ marketing_report/models/sales.py
@@ -19,12 +19,11 @@ class SalesDoc(models.Model):
     quantity = models.FloatField(default=0)
     total_sale_with_vat = models.FloatField(default=0)
     total_sale_without_vat = models.FloatField(default=0)
-    total_buy_with_vat = models.FloatField(default=0)
-    total_buy_without_vat = models.FloatField(default=0)
+    total_purchase_with_vat = models.FloatField(default=0)
+    total_purchase_without_vat = models.FloatField(default=0)
     order = models.CharField(max_length=100, null=True)
     good_no_error = models.BooleanField(default=True)
     eco = models.BooleanField(default=True)
-    week = models.ForeignKey(ReportPeriod, models.SET_NULL, null=True, default='', related_name='week')
     month = models.ForeignKey(ReportPeriod, models.SET_NULL, null=True, default='', related_name='month')
     quarter = models.ForeignKey(ReportPeriod, models.SET_NULL, null=True, default='', related_name='quarter')
     year = models.ForeignKey(ReportPeriod, models.SET_NULL, null=True, default='', related_name='year')
@@ -49,47 +48,46 @@ class SalesDoc(models.Model):
 
 class Sales(models.Model):
     """ Frigat fields from report:
-    series_id
-    good_id
-    good_group_id
-    good_group
-    good_title
-    good_name
-    code_1
-    """
+        import_date : дата импорта
+        code: артикул [1]
+        goods: деталь (ссылка)
+        color_code: код после артикула детали
+        main_color: код главного цвета
+        color: главный цвет (ссылка)
+        quantity: количество данной себестоимости [11]
+        sales_doc_no: номер документа продажи [13]
+        sales_doc_date: дата документа продажи [14]
+        purchase_without_vat: сумма без НДС закупки [15]
+        purchase_with_vat: сумма с НДС закупки [16]
+        sale_without_vat: сумма без НДС продажи [17]
+        sale_with_vat: сумма с НДС продажи [19]
+        price_vat: цена продажи с НДС [20]
+        customer_frigat_id: код клиента фрегат [21]
+        customer_name: название клиента [22]
+        customer: клиент (ссылка)
+        """
 
-    import_date = models.DateField(default=datetime.date(2000, 1, 1))
-    code = models.CharField(max_length=30)
+    import_date = models.DateField(default=datetime.date(2024, 1, 31))
+    code = models.CharField(max_length=140)
     goods = models.ForeignKey(Goods, models.SET_NULL, null=True, db_index=True)
-    color_code = models.CharField(max_length=40, null=True)
-    main_color = models.CharField(max_length=12, null=True)
+    color_code = models.CharField(max_length=60, null=True)
+    main_color = models.CharField(max_length=60, null=True)
     color = models.ForeignKey(Color, models.SET_NULL, null=True)
-    series_id = models.IntegerField(null=True)
-    good_id = models.IntegerField(null=True)
-    good_group_id = models.IntegerField(null=True)
-    good_group = models.CharField(max_length=255, null=True)
-    good_title = models.CharField(max_length=255, null=True)
-    good_name = models.CharField(max_length=255, null=True)
-    code_1 = models.CharField(max_length=100, null=True)
     quantity = models.FloatField(default=0)
-    sales_doc_name = models.CharField(max_length=255, default='Расходная накладная')
     sales_doc_no = models.CharField(max_length=20, null=True)
-    sales_doc_date = models.DateField(default=datetime.date(2000, 1, 1))
-    buy_without_vat = models.FloatField(default=0)
-    buy_with_vat = models.FloatField(default=0)
-    sales_quantity = models.FloatField(default=0)
+    sales_doc_date = models.DateField(default=datetime.date(2019, 1, 1))
+    purchase_without_vat = models.FloatField(default=0)
+    purchase_with_vat = models.FloatField(default=0)
     sale_without_vat = models.FloatField(default=0)
     sale_with_vat = models.FloatField(default=0)
-    sale_price_vat = models.FloatField(default=0)
-    customer_name = models.CharField(max_length=255, null=True)
+    price_vat = models.FloatField(default=0)
     customer_frigat_id = models.IntegerField(null=True)
-    customer_all = models.ForeignKey(Customer, models.SET_NULL, null=True)
-    sales_doc = models.ForeignKey(SalesDoc, models.SET_NULL, null=True, db_index=True)
+    customer_name = models.CharField(max_length=255, null=True)
+    customer = models.ForeignKey(Customer, models.SET_NULL, null=True)
+    no_vat = models.BooleanField(default=False)
 
     def __repr__(self):
         return self.code
 
     def __str__(self):
         return str(self.code)
-
-
