b10f623def68b1bcf664abc9fa71d2c331a36c44
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-02-10 00:04:31 +0300

    business unit added

--- marketing_report/models/goods_types_models.py
+++ marketing_report/models/goods_types_models.py
@@ -2,6 +2,13 @@ from django.db import models
 from marketing_report.models.settings_dictionary import SettingsDictionary
 
 
+class BusinessUnit(SettingsDictionary):
+    """
+    Sales Unit is a
+    """
+    code = models.IntegerField(default=0)
+
+
 class MatrixType(SettingsDictionary):
     """
     GoodMatrixType is a
@@ -12,3 +19,4 @@ class CrmType(SettingsDictionary):
     """
     GoodCrmType is a
     """
+    business_unit = models.ForeignKey(BusinessUnit, models.SET_NULL, null=True, blank=True)
