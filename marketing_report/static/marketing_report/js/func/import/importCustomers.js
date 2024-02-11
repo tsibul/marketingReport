c43dffbf25121caf9302b231ff78d2020fd826ca
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-02-11 22:49:09 +0300

    bug fix

--- marketing_report/static/marketing_report/js/func/import/importCustomers.js
+++ marketing_report/static/marketing_report/js/func/import/importCustomers.js
@@ -6,7 +6,7 @@ export async function importCustomers(btn, url) {
     btn.disabled = true;
     btn.classList.add('form-input__inactive');
     const counts = await fetchJsonData('/marketing/' + url);
-    const newCustomers = document.getElementById(url);
+    const newCustomers = btn.previousElementSibling;
     newCustomers.textContent = `импортировано ${counts.result}`;
     btn.disabled = false;
     btn.classList.remove('form-input__inactive');
