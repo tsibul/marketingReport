d2c4f5a81242ebd09e0c8031be8e0693377a0392
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-02-15 22:17:28 +0300

    Make row in prepared row list from report data taken from local storage or new report create eventListener on delete button

---
+++ marketing_report/static/marketing_report/js/func/index/reportListRow.js
@@ -0,0 +1,26 @@
+'use strict'
+
+import {deleteReport} from "./deleteReport.js";
+
+/**
+ * Make row in prepared row list from report data taken from local storage or new report
+ * create eventListener on delete button
+ * @param report - report data taken from local storage or new report
+ * @returns {HTMLDivElement} - row in row list
+ */
+export function reportListRow(report) {
+    const row = document.createElement('div');
+    row.classList.add('report-list__row');
+    const rowContent = `
+                <input type="text" value="${report.id}" hidden>
+                <div>${report.date}</div>
+                <div>${report.report.report_name}</div>
+                <div>${report.report.period}</div>
+                <div>c&nbsp;${report.report.date_begin}</div>
+                <div>c&nbsp;${report.report.date_end}</div>
+                <div>c&nbsp;${report.report.parameter}</div>
+                <button type="button" class="btn btn-save">удалить</button>`;
+    row.insertAdjacentHTML('afterbegin', rowContent);
+    row.querySelector('.btn').addEventListener('click', e => deleteReport(e.target));
+    return row;
+}
